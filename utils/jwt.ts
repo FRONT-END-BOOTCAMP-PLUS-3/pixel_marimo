bdc45b3427a88675ef73f5975508057c025afa80
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-21 17:00:10 +0900

    ✨ : 토큰을 생성하는 유틸 추가 #2

---
+++ utils/jwt.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+const SECRET_KEY = process.env.MARIMO_SECRET_KEY!
+
+export const generateJWT = (id: number, email: string) => {
+  const payload = { id, email }
+  const options = { expiresIn: 12 * 60 * 60 } // 12시간 동안 유효
+  const token = jwt.sign(payload, SECRET_KEY, options)
+  return token
+}
+
+export const verifyJWT = (token: string) => {
+  try {
+    return jwt.verify(token, SECRET_KEY)
+  } catch (error) {
+    return null // 유효하지 않거나 만료된 토큰
+  }
+}
