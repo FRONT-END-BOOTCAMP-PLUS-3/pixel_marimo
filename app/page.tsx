ca7fb703b64288ee8e6100614a9fdc6c26deb98c
MJ <alswns8495@naver.com>
2025-02-21 10:25:46 +0900

    ✨ : marimo.svg, marimo, canvas 컴포넌트 , home 페이지 생성 #1

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Canvas from "@marimo/components/canvas/page"
 const Home = () => {
   return (
     <div>
-      <h1>Marimo Img in Canvas</h1>
-      <Canvas marimoImgSrc="images/marimo.svg" />
+      <Canvas />
     </div>
   )
 }
-
 export default Home
--- components/canvas/page.tsx
+++ components/canvas/page.tsx
@@ -7,32 +7,38 @@ import styles from "./page.module.css"
 import { useStore } from "@marimo/stores/use-store"
 
 interface MarimoData {
-  id: number
-  userId: number
-  size: number
-  rect: string
-  color: string
-  status: string
+  user: {
+    id: number
+    userId: number
+    size: number
+    rect: string
+    color: string
+    status: string
+  }
 }
 
-interface CanvasProps {
-  marimoImgSrc: string
-}
-
-const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
+const Canvas = () => {
+  const marimoImgSrc = "images/marimo.svg"
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [canvasWidth, setCanvasWidth] = useState(window.innerWidth)
   const [canvasHeight, setCanvasHeight] = useState(window.innerHeight)
-  const [imageLoaded, setImageLoaded] = useState(false) // 이미지 로딩 완료 여부 추적하기 위해
+  const [imageLoaded, setImageLoaded] = useState(false)
+  // const [loadedTrashImages, setLoadedTrashImages] = useState([])
+
   const [marimoPosition, setMarimoPosition] = useState({
-    x: canvasWidth / 2 - 50,
-    y: canvasHeight / 2 - 50,
+    x: -500, // fetch 전 안보이게 하려고 넣어놓은 숫자
+    y: -500,
   })
   const [isDragging, setIsDragging] = useState(false)
-  const [startPosition, setStartPosition] = useState({ x: 0, y: 0 }) // 드래그 시작 위치 저장
-  const imageRef = useRef(new Image()) // 이미지 객체를 참조
-  const { user } = useStore()
-  const [marimoData, setMarimoData] = useState<MarimoData | null>(null) // 마리모 데이터를 상태로 저장
+  const [startPosition, setStartPosition] = useState({ x: 0, y: 0 })
+  const imageRef = useRef(new Image())
+  const { user, trashItems } = useStore()
+
+  const [marimoData, setMarimoData] = useState<MarimoData | null>(null)
+
+  // useEffect(() => {
+  //   console.log("생성된 쓰레기 배열:", trashItems)
+  // }, [trashItems])
 
   useEffect(() => {
     window.addEventListener("resize", handleCanvasResize)
@@ -45,18 +51,19 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
     setCanvasWidth(window.innerWidth)
     setCanvasHeight(window.innerHeight)
   }
-  useEffect(() => {
+
+  const loadMarimoImage = () => {
     const marimoImage = imageRef.current
     marimoImage.src = marimoImgSrc
     marimoImage.onload = () => {
       setImageLoaded(true)
     }
-  }, [marimoImgSrc])
+  }
 
-  useEffect(() => {
+  const drawMarimoOnCanvas = () => {
     if (imageLoaded && canvasRef.current) {
-      const canvas = canvasRef.current //<canvas> DOM 요소를 변수 canvas에 할
-      const ctx = canvas.getContext("2d") //2d 렌더링 컨텍스트 , 그림을 그리기 위한 API
+      const canvas = canvasRef.current
+      const ctx = canvas.getContext("2d")
       if (ctx) {
         ctx.clearRect(0, 0, canvasWidth, canvasHeight)
         ctx.drawImage(
@@ -68,7 +75,46 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
         )
       }
     }
+  }
+
+  useEffect(() => {
+    loadMarimoImage()
+  }, [marimoImgSrc])
+
+  useEffect(() => {
+    drawMarimoOnCanvas()
   }, [marimoPosition, imageLoaded, canvasWidth, canvasHeight])
+
+  // const loadTrashImages = () => {
+  //   trashItems.forEach((item) => {
+  //     const img = new Image()
+  //     img.src = item.url
+  //     img.onload = () =>
+  //       setLoadedTrashImages((prev) => [...prev, { ...item, image: img }])
+  //   })
+  // }
+
+  // // 캔버스에 쓰레기 이미지를 그리는 함수
+  // const drawTrashOnCanvas = () => {
+  //   const canvas = canvasRef.current
+  //   const ctx = canvas?.getContext("2d")
+
+  //   if (ctx) {
+  //     loadedTrashImages.forEach((trash) => {
+  //       const x = (trash.x / 100) * canvasWidth // 퍼센테이지 위치 계산
+  //       const y = (trash.y / 100) * canvasHeight
+  //       ctx.drawImage(trash.image, x, y, 50, 50) // 이미지 그리기
+  //     })
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   loadTrashImages()
+  // }, [trashItems])
+
+  // useEffect(() => {
+  //   drawTrashOnCanvas() // 캔버스에 그리기
+  // }, [loadedTrashImages, canvasWidth, canvasHeight])
 
   const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const rect = canvasRef.current?.getBoundingClientRect()
@@ -94,7 +140,6 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
 
-    // 드래그 상태와 무관하게 마우스 포인터 스타일과 로그를 처리
     if (
       x > marimoPosition.x &&
       x < marimoPosition.x + 100 &&
@@ -132,7 +177,6 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
         throw new Error("Failed to fetch data")
       }
       const data = await response.json()
-      console.log("API Response:", data)
 
       // 마리모 데이터를 상태에 저장
       setMarimoData({
@@ -143,12 +187,60 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
     }
   }
 
+  const updateMarimo = async () => {
+    const updatedRect = JSON.stringify({
+      x: (marimoPosition.x * 100) / canvasWidth,
+      y: (marimoPosition.y * 100) / canvasHeight,
+    })
+
+    // marimoData의 rect 정보를 업데이트
+    const updatedMarimoData = {
+      ...marimoData,
+      user: {
+        ...marimoData?.user,
+        rect: updatedRect,
+      },
+    }
+    const response = await fetch(`/api/marimo/update/${marimoData?.user.id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedMarimoData.user),
+    })
+    if (!response.ok) {
+      console.error("Failed to update marimo.")
+      return
+    }
+
+    const data = await response.json()
+    console.log("Marimo updated:", data)
+  }
+
+  useEffect(() => {
+    const handleBeforeUnload = async () => {
+      // 이벤트의 기본 동작을 막지 않고 업데이트 함수를 바로 호출합니다.
+      await updateMarimo()
+    }
+
+    window.addEventListener("beforeunload", handleBeforeUnload)
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload)
+    }
+  }, [updateMarimo])
+
   useEffect(() => {
     fetchMarimo()
   }, [user?.id])
 
   useEffect(() => {
-    console.log("marimoData 는 ", marimoData)
+    if (marimoData) {
+      const rectObject = JSON.parse(marimoData.user.rect)
+      const percentagedX = (canvasWidth * rectObject.x) / 100
+      const percentagedY = (canvasHeight * rectObject.y) / 100
+      setMarimoPosition({ x: percentagedX, y: percentagedY })
+    }
   }, [marimoData])
 
   return (
@@ -161,7 +253,7 @@ const Canvas: React.FC<CanvasProps> = ({ marimoImgSrc }) => {
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp} // 마우스가 캔버스를 벗어날 때 드래그 종료
+        onMouseLeave={handleMouseUp}
       />
     </div>
   )
