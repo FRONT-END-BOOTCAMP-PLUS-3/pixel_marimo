0f90c8c3e8b79fd71fba4647c2ebb772f5357e4e
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-21 17:05:43 +0900

    ⚡️ : API route 구현 #2

---
+++ app/api/user/route.ts
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
+
+import { PgUserRepository } from "@marimo/infrastructure/repositories"
+import { UserUsecase } from "@marimo/application/usecases/auth/UserUsecase"
+
+export async function GET() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get("token")?.value
+
+  if (!token)
+    return NextResponse.json({ message: "login failed" }, { status: 301 })
+
+  const usecase = new UserUsecase(new PgUserRepository())
+
+  const user = await usecase.getUser(token)
+
+  if (user === null)
+    return NextResponse.json({ message: "login failed" }, { status: 301 })
+
+  return NextResponse.json({ user }, { status: 201 })
+}
