4285bd5880d53de98d84ee7473dd54fd463626e8
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-22 20:01:43 +0900

    ✅ : hash-password에 대한 테스트 코드 추가 #2

--- utils/hash-password.ts
+++ utils/hash-password.ts
@@ -1,14 +1,16 @@
 import crypto from "crypto"
 
-const salt = process.env.MARIMO_SECRET_KEY!
-
 // 비밀번호 해싱
 export const hashPassword = (password: string) => {
+  const salt = process.env.MARIMO_SECRET_KEY!
+
   return crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex")
 }
 
 // 비밀번호 비교
 export const comparePassword = (password: string, hashedPassword: string) => {
+  const salt = process.env.MARIMO_SECRET_KEY!
+
   const hash = crypto
     .pbkdf2Sync(password, salt, 1000, 64, "sha512")
     .toString("hex")
