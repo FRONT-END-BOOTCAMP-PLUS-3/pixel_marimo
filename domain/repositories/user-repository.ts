97f523b2c7edfa8ce9fd80fe4447b9a712e0afac
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-24 14:39:42 +0900

    🔥 : prisma의 엔티티를 사용해 필요없는 파일 삭제

--- domain/repositories/user-repository.ts
+++ domain/repositories/user-repository.ts
@@ -1,5 +1,4 @@
-import { User } from "@marimo/domain/entities/User"
-
+import { User } from "@prisma/client"
 export interface UserRepository {
   findByEmail(email: string): Promise<User | null>
   findById(id: number): Promise<User | null>
