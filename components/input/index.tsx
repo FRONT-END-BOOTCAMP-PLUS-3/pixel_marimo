9b28e6f389b99b71bca06000f51f45d8312dcd66
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-26 00:06:32 +0900

    ⚡️ : pay 관련 input 속성 추가 #5

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -7,27 +7,56 @@ import {
   Dispatch,
   SetStateAction,
   useEffect,
+  KeyboardEventHandler,
 } from "react"
 
+import { formatMoney } from "@marimo/utils/format-money"
 import { getWarningMsg } from "@marimo/utils/get-warning-msg"
 
 import styles from "@marimo/components/input/index.module.css"
 
 interface InputProps {
   label: string
+  initialValue?: string
   setState: Dispatch<SetStateAction<string | undefined>>
 }
 
-export const Input: FC<InputProps> = ({ label, setState }) => {
+export const Input: FC<InputProps> = ({ label, initialValue, setState }) => {
   const { input__wrapper, input_label, input, p, isVisible } = styles
 
-  const [value, setValue] = useState<string | undefined>(undefined)
+  const [value, setValue] = useState<string | undefined>(initialValue ?? "")
   const [warning, setWarning] = useState<string | undefined>(undefined)
 
   const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const value = event?.target.value
-    value.length === 0 ? setValue(undefined) : setValue(value)
-    value.length === 0 ? setState(undefined) : setState(value)
+    if (event?.target.value.length === 0) {
+      setValue("")
+      return setState("")
+    }
+
+    const value =
+      label === "pay" ? formatMoney(event?.target.value) : event?.target.value
+
+    setValue(value)
+    setState(value)
+  }
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (
+      label === "pay" &&
+      !/^[0-9]$/.test(event.key) &&
+      ![
+        "Backspace",
+        "Tab",
+        "ArrowUp",
+        "ArrowDown",
+        "ArrowLeft",
+        "ArrowRight",
+      ].includes(event.key)
+    ) {
+      alert("숫자만 입력 할 수 있습니다.")
+
+      event.preventDefault()
+    }
   }
 
   useEffect(() => {
@@ -45,8 +74,11 @@ export const Input: FC<InputProps> = ({ label, setState }) => {
         type={label}
         id={label}
         name={label}
+        value={value}
+        onKeyDown={handleKeyDown}
         className={`${input} text-lg`}
         onChange={onChangeHandler}
+        autoComplete={label === "pay" ? "off" : "on"}
       />
       <p className={`${p} ${!!warning && isVisible} text-sm`}>{warning}</p>
     </div>
