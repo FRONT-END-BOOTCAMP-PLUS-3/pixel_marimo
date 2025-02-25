724f2dd930e9b54504cbe7d1372cf05447b28d92
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-26 00:03:28 +0900

    ✅ : route 테스트 코드 수정 및 order route 테스트 코드 추가 #5

--- __tests__/api/login/route.spec.ts
+++ __tests__/api/login/route.spec.ts
@@ -1,102 +1,105 @@
 import { NextRequest } from "next/server"
 
 import { POST } from "@marimo/app/api/login/route"
-import { test, vi, expect, beforeEach, afterEach } from "vitest"
+import { test, vi, expect, beforeEach, afterEach, describe } from "vitest"
 import { LoginUsecase } from "@marimo/application/usecases/auth/login-usecase"
 
-beforeEach(() => {
-  vi.mock("@marimo/application/usecases/auth/login-usecase", () => ({
-    LoginUsecase: vi.fn().mockImplementation(() => ({
-      execute: vi.fn(),
-    })),
-  }))
+describe("Auth Login API 테스트", () => {
+  beforeEach(() => {
+    vi.mock("@marimo/application/usecases/auth/login-usecase", () => ({
+      LoginUsecase: vi.fn().mockImplementation(() => ({
+        execute: vi.fn(),
+      })),
+    }))
 
-  vi.mock("@marimo/infrastructure/repositories", () => ({
-    PgAuthRepository: vi.fn(),
-    PgUserRepository: vi.fn(),
-  }))
-})
-
-afterEach(() => {
-  vi.clearAllMocks()
-})
-
-test("이메일 또는 비밀번호가 없으면 400 상태와 실패 메시지를 반환한다", async () => {
-  const request = {
-    json: vi.fn().mockResolvedValue({ email: "", password: "" }), // 이메일과 비밀번호가 없는 경우
-  } as unknown as NextRequest
-
-  const response = await POST(request)
-
-  expect(response.status).toBe(400)
-  expect(await response.json()).toEqual({
-    message: "이메일, 비밀번호를 입력해주세요.",
+    vi.mock("@marimo/infrastructure/repositories", () => ({
+      PgAuthRepository: vi.fn(),
+      PgUserRepository: vi.fn(),
+    }))
   })
-})
 
-test("유효하지 않은 이메일/비밀번호로 로그인 시 303 상태와 실패 메시지를 반환한다", async () => {
-  const request = {
-    json: vi.fn().mockResolvedValue({
-      email: "invalid@example.com",
-      password: "wrongpassword",
-    }), // 유효하지 않은 이메일/비밀번호
-  } as unknown as NextRequest
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
 
-  const usecaseMock = {
-    execute: vi.fn().mockRejectedValueOnce(new Error("isNotComparePassword")), // 로그인 실패 시 오류 반환
-  }
+  test("이메일 또는 비밀번호가 없으면 400 상태와 실패 메시지를 반환한다", async () => {
+    const request = {
+      json: vi.fn().mockResolvedValue({ email: "", password: "" }),
+    } as unknown as NextRequest
 
-  vi.mocked(LoginUsecase).mockReturnValue(usecaseMock)
+    const response = await POST(request)
 
-  const response = await POST(request)
+    expect(response.status).toBe(400)
+    expect(await response.json()).toEqual({
+      message: "이메일, 비밀번호를 입력해주세요.",
+    })
+  })
 
-  expect(response.status).toBe(303) // 303 상태 코드
-  expect(await response.json()).toEqual({ message: "isNotComparePassword" })
-})
+  test("유효하지 않은 이메일/비밀번호로 로그인 시 303 상태와 실패 메시지를 반환한다", async () => {
+    const request = {
+      json: vi.fn().mockResolvedValue({
+        email: "invalid@example.com",
+        password: "wrongpassword",
+      }),
+    } as unknown as NextRequest
 
-test("로그인 성공 시 200 상태와 사용자 정보를 반환한다", async () => {
-  const request = {
-    json: vi.fn().mockResolvedValue({
-      email: "test@example.com",
-      password: "correctpassword",
-    }), // 올바른 이메일/비밀번호
-  } as unknown as NextRequest
+    const usecaseMock = {
+      execute: vi.fn().mockRejectedValueOnce(new Error("isNotComparePassword")),
+    }
 
-  const authenticatedMember = { id: 1, name: "John Doe", token: "valid-token" }
+    vi.mocked(LoginUsecase).mockReturnValue(usecaseMock as any)
 
-  const usecaseMock = {
-    execute: vi.fn().mockResolvedValueOnce(authenticatedMember), // 로그인 성공 시 성공적인 반환
-  }
+    const response = await POST(request)
 
-  // LoginUsecase를 모킹하여 인스턴스를 반환
-  vi.mocked(LoginUsecase).mockReturnValue(usecaseMock)
+    expect(response.status).toBe(303)
+    expect(await response.json()).toEqual({ message: "isNotComparePassword" })
+  })
 
-  const response = await POST(request)
+  test("로그인 성공 시 200 상태와 사용자 정보를 반환한다", async () => {
+    const request = {
+      json: vi.fn().mockResolvedValue({
+        email: "test@example.com",
+        password: "correctpassword",
+      }),
+    } as unknown as NextRequest
 
-  expect(response.status).toBe(200) // 200 상태 코드
-  expect(await response.json()).toEqual(authenticatedMember)
-})
+    const authenticatedMember = {
+      id: 1,
+      name: "John Doe",
+      token: "valid-token",
+    }
 
-test("알 수 없는 오류가 발생했을 경우 500 상태와 오류 메시지를 반환한다", async () => {
-  const request = {
-    json: vi.fn().mockResolvedValue({
-      email: "test@example.com",
-      password: "correctpassword",
-    }), // 올바른 이메일/비밀번호
-  } as unknown as NextRequest
+    const usecaseMock = {
+      execute: vi.fn().mockResolvedValueOnce(authenticatedMember),
+    }
 
-  // LoginUsecase 모킹
-  const usecaseMock = {
-    execute: vi.fn().mockRejectedValueOnce(new Error("Unexpected error")), // 예상치 못한 오류를 던짐
-  }
+    vi.mocked(LoginUsecase).mockReturnValue(usecaseMock as any)
 
-  // LoginUsecase를 모킹하여 인스턴스를 반환
-  vi.mocked(LoginUsecase).mockReturnValue(usecaseMock)
+    const response = await POST(request)
 
-  const response = await POST(request)
+    expect(response.status).toBe(200)
+    expect(await response.json()).toEqual(authenticatedMember)
+  })
 
-  expect(response.status).toBe(500) // 500 상태 코드
-  expect(await response.json()).toEqual({
-    message: "Login Error: Error: Unexpected error",
+  test("알 수 없는 오류가 발생했을 경우 500 상태와 오류 메시지를 반환한다", async () => {
+    const request = {
+      json: vi.fn().mockResolvedValue({
+        email: "test@example.com",
+        password: "correctpassword",
+      }),
+    } as unknown as NextRequest
+
+    const usecaseMock = {
+      execute: vi.fn().mockRejectedValueOnce(new Error("Unexpected error")),
+    }
+
+    vi.mocked(LoginUsecase).mockReturnValue(usecaseMock as any)
+
+    const response = await POST(request)
+
+    expect(response.status).toBe(500)
+    expect(await response.json()).toEqual({
+      message: "Login Error: Error: Unexpected error",
+    })
   })
 })
