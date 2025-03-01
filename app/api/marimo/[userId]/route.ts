da72f346d3771909b54893a4242e40b936ea18e8
MJ <alswns8495@naver.com>
2025-03-01 19:10:42 +0900

    🐛 : marimo/[userId] route.ts NextAPIRequest -> NextRequest #1

--- app/api/marimo/[userId]/route.ts
+++ app/api/marimo/[userId]/route.ts
@@ -1,12 +1,11 @@
-import { NextApiRequest } from "next"
 import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 import { PgMarimoRepository } from "@marimo/infrastructure/repositories"
 import { MarimoUsecase } from "@marimo/application/usecases/marimo/marimo-usecase"
 
 export async function POST(
-  req: NextApiRequest,
+  req: NextRequest,
   { params }: { params: { userId: string } },
 ) {
   const cookieStore = await cookies()
@@ -19,7 +18,6 @@ export async function POST(
     return NextResponse.json({ message: "login failed" }, { status: 401 })
 
   const usecase = new MarimoUsecase(new PgMarimoRepository())
-
   const user = await usecase.ensureAliveMarimo(userId)
 
   if (!user || user === null)
